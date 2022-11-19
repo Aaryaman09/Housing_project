bf2ecc6794ca6501a64e2cb2f03f8b5d82675c5f
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2022-11-19 13:05:17 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from typing import List
+
+# Declaring variables for setup
+PROJECT_NAME = 'housing-predictor'
+VERSION = '0.0.1'
+AUTHOR = 'AARYAMAN_SHARMA'
+DESCRIPTION = 'THIS IS THE FIRST PROJECT'
+PACKAGES =  ["housing"]
+REQUIREMENT_FILE_NAME = 'requirements.txt'
+
+def get_requirements_list()->List[str]:
+    """
+    Description : This function is going to return list of requirement mention in requirements.txt file
+
+    This function returns a list which contains name of libraries mentioned requirements.tt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author = AUTHOR,
+    description=DESCRIPTION,
+    packages = PACKAGES,
+    install_requires = get_requirements_list()
+)

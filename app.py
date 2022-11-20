80b5106cec9256823833dd44cc65985220b2b34c
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2022-11-20 15:13:26 +0530

    Exception handling added

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','Post'])
 def index():
+    try: 
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")    
     logging.info("we are testing logging")
-    return 'Hello'
+    return 'Hello this is update'
 
 if __name__=='__main__':
     app.run(debug=True)

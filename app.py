5d74c2072cab07ec6657bdcefe29bb0725972a63
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2022-11-20 14:03:17 +0530

    Added logging

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','Post'])
 def index():
+    logging.info("we are testing logging")
     return 'Hello'
 
 if __name__=='__main__':
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 # Declaring variables for setup
 PROJECT_NAME = 'housing-predictor'
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 AUTHOR = 'AARYAMAN_SHARMA'
 DESCRIPTION = 'THIS IS THE FIRST PROJECT'
-PACKAGES =  ["housing"]
+# PACKAGES =  ["housing"]
 REQUIREMENT_FILE_NAME = 'requirements.txt'
 
 def get_requirements_list()->List[str]:
@@ -16,13 +16,13 @@ def get_requirements_list()->List[str]:
     This function returns a list which contains name of libraries mentioned requirements.tt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author = AUTHOR,
     description=DESCRIPTION,
-    packages = PACKAGES,
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
